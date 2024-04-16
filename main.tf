3fda6f47e13227bb1e33bf64a5fd2c7e2a4c9a4a
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2024-04-16 13:32:41 +0900

    Add tutorial level file with functions converted to terraform

---
+++ main.tf
@@ -0,0 +1,66 @@
+provider "google" {
+  project = "ea-yukihidemitsuoka"
+  region  = "asia-northeast1"
+
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "functions_helloworld"
+  }
+}
+
+resource "random_id" "default" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name                        = "${random_id.default.hex}-gcf-source" # Every bucket name must be globally unique
+  location                    = "ASIA-NORTHEAST1"
+  uniform_bucket_level_access = true
+}
+
+data "archive_file" "default" {
+  type        = "zip"
+  output_path = "/tmp/function-source.zip"
+  source_dir  = "./"
+}
+resource "google_storage_bucket_object" "object" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.default.name
+  source = data.archive_file.default.output_path # Add path to the zipped function source code
+}
+
+resource "google_cloudfunctions2_function" "default" {
+  name        = "function-v2"
+  location    = "asia-northeast1"
+  description = "a new function"
+
+  build_config {
+    runtime     = "python311"
+    entry_point = "hello_http" # Set the entry point
+    source {
+      storage_source {
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.default.location
+  service  = google_cloudfunctions2_function.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+output "function_uri" {
+  value = google_cloudfunctions2_function.default.service_config[0].uri
+}
